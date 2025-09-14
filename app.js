12a655ddcabaa071ba750aeb2611a386d5fcf0b1
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-09-14 20:14:53 +0200

    put basic operator functions in place

---
+++ app.js
@@ -0,0 +1,25 @@
+const add = function(a, b) {
+  return a + b;
+}
+
+const subtract = function(a, b) {
+  return a - b;
+}
+
+const multiply = function(a, b) {
+  return a * b;
+}
+
+const divide = function(a, b) {
+  return a / b;
+}
+
+const num1;
+const operator;
+const num2;
+
+const operate = function(num1, operator, num2) {
+  return num1 + operator + num2;
+}
+
+
