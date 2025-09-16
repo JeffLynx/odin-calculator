f4202de6a1733d6df983e9e43eac8540e6203b60
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-09-16 15:33:35 +0200

    Add basic input functionality to the buttons as well as a button for 'clear all' and 'clear one'

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-const add = function(a, b) {
-  return a + b;
+const add = function(num1, num2) {
+  return num1 + num2;
 }
 
-const subtract = function(a, b) {
-  return a - b;
+const subtract = function(num1, num2) {
+  return num1 - num2;
 }
 
-const multiply = function(a, b) {
-  return a * b;
+const multiply = function(num1, num2) {
+  return num1 * num2;
 }
 
-const divide = function(a, b) {
-  return a / b;
+const divide = function(num1, num2) {
+  return num1 / num2;
 }
 
-const num1;
-const operator;
-const num2;
+let num1;
+let operator;
+let num2;
 
-const operate = function(num1, operator, num2) {
-  return num1 + operator + num2;
+const operate = function(operator, num1, num2) {
+  return operator(num1, num2);
 }
 
+const calcScreen = document.querySelector(".calcScreen");
+const numBtns = document.querySelectorAll(".numBtn");
+const opBtns = document.querySelectorAll(".opBtn");
+const clearAllBtn = document.querySelector(".clearAllBtn");
+const clearOneBtn = document.querySelector(".clearOneBtn");
 
+
+numBtns.forEach((numBtn) => {
+  numBtn.addEventListener("click", () => {
+    calcScreen.textContent += numBtn.textContent;
+  });
+});
+
+opBtns.forEach((opBtn) => {
+  opBtn.addEventListener("click", () => {
+    calcScreen.textContent += opBtn.textContent;
+  });
+});
+
+clearAllBtn.addEventListener("click", () => {
+  calcScreen.textContent = "";
+});
+
+clearOneBtn.addEventListener("click", () => {
+  calcScreen.textContent = calcScreen.textContent.slice(0, -1);
+});
